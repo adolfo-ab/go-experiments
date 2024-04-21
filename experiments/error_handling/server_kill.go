d81ef6b4830da84413286841ef9fd223a82347c8
adolfo-ab <adolfoab6@gmail.com>
2024-04-21 11:10:02 +0200

    Add error handling experiments

--- experiments/error_handling/server_kill.go
+++ experiments/error_handling/server_kill.go
@@ -1,1 +1,30 @@
-package error_handling
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func killServer(pidFile string) error {
+	data, err := os.ReadFile(pidFile)
+	if err != nil {
+		return err
+	}
+
+	pid, err := strconv.Atoi(string(data))
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("killing server with pid=%d\n", pid)
+	return nil
+}
+
+func main() {
+	if err := killServer("experiments/error_handling/server.pid"); err != nil {
+		log.Fatalf("error: %s", err)
+		os.Exit(1)
+	}
+}
