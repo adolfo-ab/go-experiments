9ed612e2edc4110ad653091b99b1d95645067089
adolfo-ab <adolfoab6@gmail.com>
2024-04-22 08:07:45 +0200

    Playing around with the testing library

---
+++ experiments/testing/fibonacci_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFibonacci(t *testing.T) {
+	expectedValues := []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}
+	f := fibonacci()
+
+	for i := 0; i < 10; i++ {
+		val := f()
+		if val != expectedValues[i] {
+			t.Fatalf("Wrong value. Expected %d, got %d.", expectedValues[i], val)
+		}
+
+	}
+
+}
+
+type testCase struct {
+	n        int
+	expected int
+}
+
+func TestFibonacciByFormula(t *testing.T) {
+	testCases := []testCase{
+		{0, 0},
+		{15, 610},
+		{25, 75025},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", tc.n), func(t *testing.T) {
+			val := fibonacciByFormula(tc.n)
+			if val != tc.expected {
+				t.Fatalf("Wrong value. Expected %d, got %d.", tc.expected, val)
+			}
+		})
+	}
+}
