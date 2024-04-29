c9dc1007d3e9d1e10ea7ae904b93750895ac7333
adolfo-ab <adolfoab6@gmail.com>
2024-04-29 08:25:10 +0200

    Experiments on auth and tls

--- experiments/security/tls/httpd.go
+++ experiments/security/tls/httpd.go
@@ -1,1 +1,20 @@
-package tls
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "OK\n")
+}
+
+func main() {
+	http.HandleFunc("/health", healthHandler)
+
+	if err := http.ListenAndServeTLS(":8443", "./experiments/security/tls/cert.pem", "./experiments/security/tls/key.pem", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
